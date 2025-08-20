b862876df9937e4af95303699271180958e27f68
nica <nica.dable@gmail.com>
2025-08-20 23:46:04 +0800

    add basic footer

---
+++ diwanag-2025-app/app/components/Footer.tsx
@@ -0,0 +1,10 @@
+import Link from "next/link";
+
+export default function Footer() {
+  return (
+    <footer>
+      &copy; {new Date().getFullYear()} Atenews Diwanag 2025. All rights
+      reserved.
+    </footer>
+  );
+}
