1da84adf26d15c99bb8cbbb3c2664068e7f3ec6f
nica <nica.dable@gmail.com>
2025-08-30 15:47:35 +0800

    feat: add averia serif as fonts to navbar, footer, and page layout

--- diwanag-2025-app/app/components/Navbar.tsx
+++ diwanag-2025-app/app/components/Navbar.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 export default function Navbar() {
   return (
-    <nav>
+    <nav className="averia-serif">
       <ul
         style={{
           display: "flex",
